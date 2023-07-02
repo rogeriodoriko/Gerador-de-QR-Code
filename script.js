2829ec857bbf4af056871376f99ca73581ce0a27
Rogério Fernandes <112347141+rogeriodoriko@users.noreply.github.com>
2023-07-02 17:37:58 -0300

    v1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+function generateQRCode() {
+  var inputData = document.getElementById("inputData").value;
+  if (!inputData) {
+    alert("Por favor, digite um texto ou link válido.");
+    return;
+  }
+
+  var qrcodeContainer = document.getElementById("qrcode");
+  qrcodeContainer.innerHTML = "";
+
+  var qrcode = new QRCode(qrcodeContainer, {
+    text: inputData,
+    width: 256,
+    height: 256,
+  });
+}
